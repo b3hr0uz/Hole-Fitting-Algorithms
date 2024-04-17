5cbf231049ceb5b5df9795f2dc08931699ca079c
Behrouz Barati B <b3h@me.com>
2024-04-16 19:26:18 -0700

    Initial Commit

---
+++ Hole-Fitting-Algorithms/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hole-Fitting-Algorithms
+//
+//  Created by Behrouz Barati on 4/16/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
